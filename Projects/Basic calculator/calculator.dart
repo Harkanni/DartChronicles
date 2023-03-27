3b89cf78304b886af59a6873bfec877497644907
harkanni <harkanzyempire001@gmail.com>
2023-03-27 12:46:07 +0100

    new file:   Projects/Basic calculator/Calculator.md new file:   Projects/Basic calculator/calculator.dart

---
+++ Projects/Basic calculator/calculator.dart
@@ -0,0 +1,10 @@
+import "dart:io";
+import "dart:math";
+
+void main() {
+  // This program reads two input from the user and prints the sum. 
+  print("Enter first number");
+  double num1 = double.parse(stdin.readLineSync()!);
+  print("Enter second number");
+  double num2 = double.parse(stdin.readLineSync()!);
+}
