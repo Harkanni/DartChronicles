798c7a29550a10135e12eaa9211f09a3463d7046
harkanni <harkanzyempire001@gmail.com>
2023-03-29 07:10:01 +0100

    new file:   Projects/Math Quiz/quiz.dart

---
+++ Projects/Basic calculator/MadLib/Madlib.dart
@@ -0,0 +1,33 @@
+import "dart:io";
+
+void main() {
+  print("Enter a color");
+  String color = stdin.readLineSync()!;
+
+  print("Enter a plural pronoun");
+  String pluralPronoun = stdin.readLineSync()!;
+
+  print("Enter a celebrity");
+  String celebrity = stdin.readLineSync()!;
+
+  print("Roses are ${color}");
+  print("${pluralPronoun} are blue");
+  print("I love ${celebrity}");
+}
+
+void sayHi(String username, int age) {
+  print("Hi ${username}, you are awesome and ${age}yrs old");
+}
+
+void abuse(String name) {
+  print("Shut the fuck up ${name}!!");
+}
+
+void main() {
+  sayHi("Joe Bidden", 30);
+  sayHi("Joe Budden", 28);
+
+  print("What's your name bud");
+  String name = stdin.readLineSync()!;
+  abuse(name);
+}
---
+++ Projects/Math Quiz/quiz.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+class MathQuestion {
+  late String question;
+  late double answer;
+
+  MathQuestion(String question, double answer){
+    this.question = question;
+    this.answer = answer;
+  }
+
+  double promptDouble(String promptText){
+    print(promptText);
+    double myNum = double.parse(stdin.readLineSync()!);
+    return myNum;
+  }
+}
+
+void main() {
+  List<MathQuestion> Questions = [
+    MathQuestion("3 + 3", 6.0),
+    MathQuestion("3 * 2", 6.0),
+    MathQuestion("4 + 2", 6.0)
+  ];
+
+ int result = 0;
+  for(MathQuestion question in Questions){
+    double userAnswer = question.promptDouble(question.question);
+    if(userAnswer == question.answer){
+      print("Correct!");
+      result++;
+    } 
+    else {
+      print("Incorrect! answer was ${question.answer}");
+    }
+  }
+  print("You scored ${result} out of ${Questions.length}");
+  stdout.write("Done!");
+}
