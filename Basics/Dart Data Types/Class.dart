3bffab78436be8b749dc85c014c7d099f8056afb
harkanni <harkanzyempire001@gmail.com>
2023-04-04 06:59:26 +0100

    modified:   Basics/Dart Data Types/Class.dart

--- Basics/Dart Data Types/Class.dart
+++ Basics/Dart Data Types/Class.dart
@@ -1,9 +1,20 @@
 void main() {
   User myUser = new User();
-  print(myUser);
+  print(myUser.name);
+
+  final myUser2 = new User();
+  myUser2.name = 'Jane Doe';
+  print(myUser2.name);
 }
 
 class User {
   String name = "John Doe";
   String photoUrl = "https://conglo.merate";
+}
+
+class User2 {
+  String? name;
+  String? photoUrl;
+
+  User2({this.name, this.photoUrl});
 }
