3aa674606ce7180bf0f69062bffce7ddb226cbe5
Akanni Emmanuel <67793955+Harkanni@users.noreply.github.com>
2023-05-07 23:18:13 +0100

    Update calculator.dart

--- Projects/Basic calculator/calculator.dart
+++ Projects/Basic calculator/calculator.dart
@@ -3,8 +3,8 @@ import "dart:math";
 
 void main() {
   // This program reads two input from the user and prints the sum.
-  print("Enter first number");
+  print("Enter first double number");
   double num1 = double.parse(stdin.readLineSync()!);
-  print("Enter second number");
+  print("Enter second double number");
   double num2 = double.parse(stdin.readLineSync()!);
 }
